0fd73b644e35d4e8a09db51d8c6841cad1d63eed
tegarsetiawicaksono <112869398+tegarsetiawicaksono@users.noreply.github.com>
2022-09-06 10:40:49 +0700

    Add files via upload

---
+++ Latihan 1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama          : Tegar Setia W" << endl;
+    cout << "Nim           : A11.2022.14634" << endl;
+    cout << "Nama Sekolah  : SMA Negeri 1 Suruh" << endl;
+    return 0;
+}
